90ba750e740a93fb2c99754ef5e2b0143eb8e546
behnam <beehnam.esmaili@gmail.com>
2022-04-29 13:48:00 +0430

    add Admin package

---
+++ packages/admin/pages/index.tsx
@@ -0,0 +1,15 @@
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return (
+    <div>
+     Welcome To Atomic Web Admin
+    </div>
+  );
+}
+
+export default Index;
