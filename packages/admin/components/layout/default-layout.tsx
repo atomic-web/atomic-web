0ae068d4b41f5d1e8e2194bf0905de75a69f77bd
behnam <beehnam.esmaili@gmail.com>
2022-05-02 23:08:57 +0430

    bugfix: in admin theme layout

--- packages/admin/components/layout/default-layout.tsx
+++ packages/admin/components/layout/default-layout.tsx
@@ -3,10 +3,15 @@ import { Header } from './header';
 import { SideBar } from './sidebar';
 import { atomicTheme } from '../../themes';
 import { ApplicationProvider, useApplication } from '../../context';
+import styled from 'styled-components';
 
 export interface LayoutProps {
   children: React.ReactChild;
 }
+
+const Body = styled(Box)`
+  height: 100vh;
+`;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface DefaulLayoutProps extends LayoutProps {}
@@ -19,20 +24,24 @@ const DefaultLayout = (props) => {
     <Grommet full theme={atomicTheme} dir={themeDir} themeMode={themeMode}>
       <Box direction="row" fill>
         <SideBar />
-        <Box flex>
+        <Body flex>
           <Header />
-          <Main background="background-back" pad="medium">
-            {children}
-          </Main>
-        </Box>
+          <Box flex overflow="auto">
+            <Main background="background-back" pad="medium">
+              <div>
+              {children}
+              </div>
+            </Main>
+          </Box>
+        </Body>
       </Box>
     </Grommet>
   );
 };
 
-const LayoutWithAppProvider = () => (
+const LayoutWithAppProvider = ({ children }) => (
   <ApplicationProvider>
-    <DefaultLayout />
+    <DefaultLayout>{children}</DefaultLayout>
   </ApplicationProvider>
 );
 
