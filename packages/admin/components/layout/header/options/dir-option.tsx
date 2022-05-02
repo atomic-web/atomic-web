9dbaf05f081b6202d5c9305288b8a832739010aa
behnam <beehnam.esmaili@gmail.com>
2022-05-02 21:59:05 +0430

    improve options UI

--- packages/admin/components/layout/header/options/dir-option.tsx
+++ packages/admin/components/layout/header/options/dir-option.tsx
@@ -1,28 +1,36 @@
 import { Box, RadioButtonGroup } from 'grommet';
+import { FormNextLink, FormPreviousLink } from 'grommet-icons';
 import { Translate } from 'next-translate';
 import { useMemo } from 'react';
 import { useApplication } from '../../../../context';
 import { OptionItem } from './options';
+import { RadioOption } from './radio-option';
 
 const DirEditor: React.FC<unknown> = () => {
   const { themeDir, toggleThemeDir } = useApplication();
 
-  const value = useMemo(()=>themeDir === 'rtl' ? 'RTL' : 'LTR',[themeDir]);
+  const value = useMemo(() => (themeDir === 'rtl' ? 'RTL' : 'LTR'), [themeDir]);
+  const optionIconMap = {
+      'RTL' : FormPreviousLink,
+      'LTR' : FormNextLink
+  }  
 
   return (
     <Box>
-        <RadioButtonGroup
-          value={value}
-          direction='row'
-          name="theme-dir"
-          onChange={toggleThemeDir}
-          options={['LTR',"RTL"]}
-        />
+      <RadioButtonGroup
+        value={value}
+        direction="row"
+        name="theme-dir"
+        onChange={toggleThemeDir}
+        options={['LTR', 'RTL']}
+      >
+          {(option , params)=>RadioOption(option,params , (option)=>optionIconMap[option])}
+      </RadioButtonGroup>
     </Box>
   );
 };
 
 export const DirOption = (t: Translate): OptionItem => ({
   title: t('options-dir'),
-  editor: <DirEditor/>,
+  editor: <DirEditor />,
 });
---
+++ packages/admin/components/layout/header/options/radio-option.tsx
@@ -0,0 +1,13 @@
+import { Box } from "grommet";
+
+export const RadioOption = (option, { checked, focus, hover }, icon) => {
+  const Icon = icon(option);
+  let background;
+  if (checked) background = 'brand';
+  else if (hover) background = 'light-4';
+  else if (focus) background = 'light-4';
+  else background = 'light-2';
+  return <Box background={background} pad="xsmall">
+      <Icon/>
+  </Box>;
+};
