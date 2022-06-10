da6775e1276db6b3a6ce89140ae068eb3f69b5d9
behnam <beehnam.esmaili@gmail.com>
2022-06-10 22:58:31 +0430

    upgrade dependencies

--- packages/admin/jest.config.ts
+++ packages/admin/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'admin',
   preset: '../../jest.preset.js',
   transform: {
--- packages/ui-core/jest.config.ts
+++ packages/ui-core/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'ui-core',
   preset: '../../jest.preset.js',
   transform: {
